c42fffb2b5cf112450a939350e89b44cac3bbd37
Son Fang <sonphan18071999@gmail.com>
2025-03-02 19:42:05 +0700

    Auditted responsive for text and highligh character

--- nextjs-landing-page-sonphan/src/components/commons/float-character/float-character.tsx
+++ nextjs-landing-page-sonphan/src/components/commons/float-character/float-character.tsx
@@ -1,25 +1,35 @@
 import styles from "@/styles/banner.module.scss";
 import React from "react";
-import { Character } from "@/models/character";
+import {Character} from "@/models/character";
+import {Carousel} from "antd";
 
-interface FloatCharacterProps {
-  character: Character;
-}
 
-const FloatCharacter = ({ character }: FloatCharacterProps) => {
-  return (
-    <section className={styles.character__container} key={character.name}>
-      <div className={styles.character}>
-        <img
-          className={styles.character__image}
-          src={character.image}
-          alt="Top character"
-          width="100%"
-        />
-      </div>
-      <p className="text-black block">{character.name}</p>
-    </section>
-  );
+const FloatCharacter = () => {
+    const characters: Character[] = [
+        {name: "Assassin", image: "assets/images/assassin.svg"},
+        {name: "Neon guy", image: "assets/images/neon-guy.svg"},
+        {name: "Mafia england", image: "assets/images/mafia-england.svg"},
+        {name: "Basketball guy", image: "assets/images/basket-ball.svg"},
+    ];
+    return (
+        <>
+            <div className={styles.character_item_flex}>
+                {characters.map((item) => <section key={item.name}
+                >
+                    <div className={styles.character}>
+                        <img
+                            className={styles.character__image}
+                            src={item.image}
+                            alt="Top character"
+                            width="100%"
+                        />
+                    </div>
+                    <div className={'flex justify-center py-2'}>{item.name}</div>
+                </section>)}
+            </div>
+
+        </>
+    );
 };
 
 export default FloatCharacter;
--- nextjs-landing-page-sonphan/src/components/features/banner/banner.tsx
+++ nextjs-landing-page-sonphan/src/components/features/banner/banner.tsx
@@ -1,57 +1,44 @@
 "use client";
 import React from "react";
 import styles from "@/styles/banner.module.scss";
-import { Character } from "@/models/character";
+import {Character} from "@/models/character";
 import FloatCharacter from "@/components/commons/float-character/float-character";
+import {Carousel} from "antd";
 
 const Banner = () => {
-  const characters: Character[] = [
-    { name: "Assassin", image: "assets/images/assassin.svg" },
-    { name: "Neon guy", image: "assets/images/neon-guy.svg" },
-    { name: "Mafia england", image: "assets/images/mafia-england.svg" },
-    { name: "Basketball guy", image: "assets/images/basket-ball.svg" },
-  ];
 
-  return (
-    <>
-      <section className={styles.banner__container}>
-        <section className={styles.banner__contents}>
-          <section className={styles.textBlock}>
-            <div className={styles.textBlock__new}>
-              <div>
-                <img
-                  src={"assets/images/new__tag.svg"}
-                  alt={"New text"}
-                  width={"100%"}
-                  height={"auto"}
-                  className={styles.textBlock__newTag__image}
-                />
-              </div>
-              <div className={styles.textBlock__shopNowContainer}>
-                <button className={styles.textBlock__shopNowBtn}></button>
-              </div>
-            </div>
-            <div className={styles.textBlock__arrival}></div>
-          </section>
 
-          <section className={styles.footer__background}></section>
+    return (
+        <>
+            <section className={styles.banner__container}>
+                <section className={styles.banner__contents}>
+                    <section className={styles.textBlock}>
+                        <div className={styles.textBlock__new}>
+                            <img
+                                src={"assets/images/new__tag.svg"}
+                                alt={"New text"}
+                                width={"100%"}
+                                height={"auto"}
+                                className={styles.textBlock__newTag__image}
+                            />
+                            <div className={styles.textBlock__shopNowContainer}>
+                                <button className={styles.textBlock__shopNowBtn}></button>
+                            </div>
+                        </div>
+                        <div className={styles.textBlock__arrival}></div>
+                        <div className={styles.highlight__character}></div>
+                    </section>
 
-          <section className={styles.characters__contents}>
-            <div className={styles.characters__list}>
-              {characters.map((character: Character) => (
-                <FloatCharacter character={character} key={character.name} />
-              ))}
-            </div>
-          </section>
+                    {/*<section className={styles.footer__background}></section>*/}
 
-          <section>
-            <div id="the-dj" className={styles.highlight__character}></div>
-          </section>
-        </section>
-        <section className={styles.characters} />
-      </section>
-    </>
-  );
+                    {/*<section className={styles.characters__contents}>*/}
+                    {/*    <FloatCharacter/>*/}
+                    {/*</section>*/}
+
+                </section>
+            </section>
+        </>
+    );
 };
 
 export default Banner;
